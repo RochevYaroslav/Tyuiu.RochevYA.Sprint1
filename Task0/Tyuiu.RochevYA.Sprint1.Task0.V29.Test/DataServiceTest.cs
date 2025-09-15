6d2fb82b90b5486a073c45a14dce312470785545
RochevYaroslav <justmisterstone@gmail.com>
2025-09-15 22:41:45 +0500

    Task0

--- Task0/Tyuiu.RochevYA.Sprint1.Task0.V29.Test/DataServiceTest.cs
+++ Task0/Tyuiu.RochevYA.Sprint1.Task0.V29.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.RochevYA.Sprint1.Task0.V29.Test
         {
             DataService ds = new DataService();
             var res = ds.Calculate();
-            Assert.AreEqual(2, res);
+            Assert.AreEqual(32, res);
         }
     }
 }
