429b0c872378a0972658ddb2e9ff1e96f1b74a78
RochevYaroslav <justmisterstone@gmail.com>
2025-09-19 19:02:10 +0500

    Task0

---
+++ Task1/Tyuiu.RochevYA.Sprint1.Task1.V28.Lab/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.RochevYA.Sprint1.Task1.V28.Lab
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Task1/Tyuiu.RochevYA.Sprint1.Task1.V28.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.RochevYA.Sprint1.Task1.V28.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
