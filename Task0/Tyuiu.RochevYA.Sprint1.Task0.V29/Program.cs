ed89a72971cdbd8db5c5c9ded2ac1f559753d18f
RochevYaroslav <justmisterstone@gmail.com>
2025-09-21 10:42:09 +0500

    Добавьте файлы проекта.

---
+++ Task0/Tyuiu.RochevYA.Sprint1.Task0.V29/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.RochevYA.Sprint1.Task0.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("2*9+7*2");
+        Console.WriteLine(ds.Calculate());
+        Console.ReadLine();
+    }
+}
