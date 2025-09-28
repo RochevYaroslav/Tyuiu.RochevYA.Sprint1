3247a0766a1f10fc8b06586d8e545098d330aba9
RochevYaroslav <justmisterstone@gmail.com>
2025-09-28 22:27:50 +0500

    Task7

---
+++ Tyuiu.RochevYA.Sprint1.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.RochevYA.Sprint1.Task7.V18.Lib;
+namespace Tyuiu.RochevYA.Sprint1.Task7.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(90, 0);
+            Console.WriteLine(res);
+            Assert.AreEqual(90.02, res);
+        }
+    }
+}
---
+++ Tyuiu.RochevYA.Sprint1.Task7.V18/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.RochevYA.Sprint1.Task7.V18.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("Число Х:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Число Y:");
+        double y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(ds.Calculate(x, y));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
